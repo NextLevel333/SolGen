0963070d60d8c4c5bd726a1a0c8022b288643033
SirNext <112789496+NextLevel333@users.noreply.github.com>
2026-02-15 15:54:26 -0500

    Adjust logo width calculation in Logo component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -11,7 +11,7 @@ export const Logo: React.FC<LogoProps> = ({ className = '', size = 40 }) => {
     <Image
       src="/SolGenLogo.svg"
       alt="SolGen Logo"
-      width={size * .9}
+      width={size * 1}
       height={size}
       className={`object-contain drop-shadow-[0_0_5px_rgba(255,255,255,0.9)] ${className}`}
       priority
