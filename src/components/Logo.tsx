09ed1c0bba1755f760fd9391460e3338c3796bae
SirNext <112789496+NextLevel333@users.noreply.github.com>
2026-02-15 15:54:01 -0500

    Change logo source to SVG and update className
    Updated logo source from PNG to SVG format and removed animation.

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -9,11 +9,11 @@ interface LogoProps {
 export const Logo: React.FC<LogoProps> = ({ className = '', size = 40 }) => {
   return (
     <Image
-      src="/SolGenLogo.png"
+      src="/SolGenLogo.svg"
       alt="SolGen Logo"
       width={size * .9}
       height={size}
-      className={`object-contain drop-shadow-[0_0_5px_rgba(255,255,255,0.9)] animate-pulse ${className}`}
+      className={`object-contain drop-shadow-[0_0_5px_rgba(255,255,255,0.9)] ${className}`}
       priority
       style={{ objectFit: 'contain' }}
     />
